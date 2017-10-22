e36c563dbd39596ef7e1cfed02ba6ce5195bc40a
Evan Turner <evturn@gmail.com>
2017-10-22 00:58:53 -0400

    Add route and handler

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,17 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
+
+import Web.Firefly
+import qualified Data.Text as T
+
+app :: App ()
+app = do
+  route "/hello" helloHandler
+
+helloHandler :: Handler T.Text
+helloHander = do
+  name <- fromMaybe "Stranger" <$> getQuery "name"
+  return $ "Hello " `T.append` name
 
 main :: IO ()
 main = do
