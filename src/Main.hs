587d622ac35b8f6abd7706de3e84f11260f4c64e
Evan Turner <evturn@gmail.com>
2017-10-22 02:03:26 -0400

    Add root handler

--- src/Main.hs
+++ src/Main.hs
@@ -7,11 +7,16 @@ import Data.Maybe
 app :: App ()
 app = do
   route "/hello" helloHandler
+  route "/"      splatHandler
 
 helloHandler :: Handler T.Text
 helloHandler = do
   name <- fromMaybe "Stranger" <$> getQuery "name"
   return $ "Hello " `T.append` name
+
+splatHandler :: Handler T.Text
+splatHandler = do
+  return $ "Sup " `T.append` "dog."
 
 main :: IO ()
 main = run 3000 app
